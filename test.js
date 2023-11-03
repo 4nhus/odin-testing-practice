cc67ba6ace2c8a11377af5dbfd81d6c48a223910
Anh Nguyen <121279445+4nhus@users.noreply.github.com>
2023-11-03 16:52:03 +1100

    Add tests and implementation of reverseString

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 import capitalise from "./capitalise";
+import reverseString from "./reverse-string";
 
 describe("capitalise", () => {
   test("empty string", () => {
@@ -16,5 +17,34 @@ describe("capitalise", () => {
   test("symbols string", () => {
     expect(capitalise("!@#$")).toBe("!@#$");
   });
-  test("test", () => {});
+});
+
+describe("reverseString", () => {
+  test("empty string", () => {
+    expect(reverseString("")).toBe("");
+  });
+  test("odd length palindromic string", () => {
+    expect(reverseString("bob")).toBe("bob");
+  });
+  test("even length palindromic string", () => {
+    expect(reverseString("boob")).toBe("boob");
+  });
+  test("odd length non palindromic string", () => {
+    expect(reverseString("thing")).toBe("gniht");
+  });
+  test("even length non palindromic string", () => {
+    expect(reverseString("four")).toBe("ruof");
+  });
+  test("odd length palindromic numeric string", () => {
+    expect(reverseString("101")).toBe("101");
+  });
+  test("even length palindromic numeric string", () => {
+    expect(reverseString("1221")).toBe("1221");
+  });
+  test("odd length non palindromic numeric string", () => {
+    expect(reverseString("12345")).toBe("54321");
+  });
+  test("even length non palindromic numeric string", () => {
+    expect(reverseString("1234")).toBe("4321");
+  });
 });
